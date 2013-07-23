d97305460ca328f71f8d139b466469ea4e2ee35f
Apprentice <apprentice@devbootcamp.com>
2013-07-23 09:41:22 -0700

    initial commit with rspec annotate and better_errors

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SweetRailsArticles::Application.config.secret_key_base = '580fe254d49bbea6ef55f983f3537e4ac9896f18c84a8c467e6a55abfad2bf31f2c424f5d3b407ecde8f1cc9bae92f6c8ae8f45a7db65b2d754792d7af7eb88a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SweetRailsArticles::Application.config.session_store :cookie_store, key: '_sweet_rails_articles_session'
